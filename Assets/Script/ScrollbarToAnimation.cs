6f94997a3a78a996ce0510bdab18bf3388549d14
tsubasamusu <tsubasa_03_08@icloud.com>
2023-05-29 06:05:40 +0900

    「ScrollbarToAnimation.cs」にコメントを追加した。

---
+++ Assets/Script/ScrollbarToAnimation.cs
@@ -0,0 +1,66 @@
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// スクロールバーをもとにアニメーションを制御する
+/// </summary>
+public class ScrollbarToAnimation : MonoBehaviour
+{
+    /// <summary>
+    /// ベッドのアニメーター
+    /// </summary>
+    [SerializeField]
+    private Animator bedAnimator;
+
+    /// <summary>
+    /// スクロールバー
+    /// </summary>
+    [SerializeField]
+    private Scrollbar scrollbar;
+
+    /// <summary>
+    /// スクロールバーが操作されているかどうか
+    /// </summary>
+    private bool scrollbarIsInteract;
+
+    /// <summary>
+    /// ゲーム開始直後に呼び出される
+    /// </summary>
+    private void Start()
+    {
+        //スクロールバーの値が変更された際に呼び出されるメソッドを登録する
+        scrollbar.onValueChanged.AddListener(OnScrollbarValueChange);
+
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                //アニメーションの状態を取得する
+                AnimatorStateInfo stateInfo = bedAnimator.GetCurrentAnimatorStateInfo(0);
+
+                //スクロールバーが操作されていないなら、スクロールバーの値を更新する
+                if (!scrollbarIsInteract) scrollbar.value = stateInfo.normalizedTime % 1;
+
+                //1本指で触れていないなら、スクロールバーが操作されていない状態に切り替える
+                if (!Input.GetMouseButton(0)) scrollbarIsInteract = false;
+            })
+            .AddTo(this);
+    }
+
+    /// <summary>
+    /// スクロールバーの値が変更された際に呼び出される
+    /// </summary>
+    /// <param name="value"></param>
+    private void OnScrollbarValueChange(float value)
+    {
+        //スクロールバーが操作されている状態に切り替える
+        scrollbarIsInteract = true;
+
+        //アニメーションの状態を取得する
+        AnimatorStateInfo stateInfo = bedAnimator.GetCurrentAnimatorStateInfo(0);
+
+        //スクロールバーの値に対応した場所からアニメーションを再生する
+        bedAnimator.Play(stateInfo.fullPathHash, -1, value);
+    }
+}
