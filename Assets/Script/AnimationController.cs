38c649d3dfbb6a4607adb7938955e0d314c0cfda
tsubasamusu <tsubasa_03_08@icloud.com>
2023-05-28 13:05:39 +0900

    「UniRX」をインポートした。

--- Assets/Script/AnimationController.cs
+++ Assets/Script/AnimationController.cs
@@ -2,54 +2,68 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-    public Animator animator;
+    [SerializeField]
+    private Animator bedAnimator;
+
     private float defaultSpeed;
-    public float speed;
-    private bool isReturn; 
-    public string animationName; // Name of the animation clip
-    public float speedUpVelocity = 1.5f;
-    public float speedDownVelocity = 1.5f;
+
+    [SerializeField]
+    private float speed;
+
+    private bool isReturn;
+
+    /// <summary>
+    /// アニメーションの名前
+    /// </summary>
+    [SerializeField]
+    private string animationName;
+
+    [SerializeField]
+    private float speedUpVelocity = 1.5f;
+
+    [SerializeField]
+    private float speedDownVelocity = 1.5f;
 
     private void Start()
     {
-        if (animator == null)
+        if (bedAnimator == null)
         {
             Debug.LogError("Animator is not assigned");
             return;
         }
-        defaultSpeed = animator.speed;
-        animator.speed = 0; // initially stop the animation
+        defaultSpeed = bedAnimator.speed;
+        bedAnimator.speed = 0; // initially stop the animation
     }
 
     public void Update()
     {
-        speed = animator.speed;
+        speed = bedAnimator.speed;
     }
 
     public void StartAnimation()
     {
         isReturn = false;
-        animator.SetFloat("Speed",1f); 
-        animator.speed = defaultSpeed;
+        bedAnimator.SetFloat("Speed",1f); 
+        bedAnimator.speed = defaultSpeed;
     }
 
     public void StopAnimation()
     {
-        animator.speed = 0;
+        bedAnimator.speed = 0;
     }
 
     public void SpeedUpAnimation()
     {
         isReturn = false;
-        animator.SetFloat("Speed",1f); 
-        animator.speed = speedUpVelocity * defaultSpeed;
+        bedAnimator.SetFloat("Speed",1f); 
+        bedAnimator.speed = speedUpVelocity * defaultSpeed;
     }
 
     public void SlowDownAnimation()
     {
         isReturn = false;
-        animator.SetFloat("Speed",1f); 
-        animator.speed = defaultSpeed / speedDownVelocity;
+        bedAnimator.SetFloat("Speed",1f); 
+        bedAnimator.speed = defaultSpeed / speedDownVelocity;
     }
 
     public void ReturnAnimation()
@@ -57,14 +71,14 @@ public class AnimationController : MonoBehaviour
         if (!isReturn)
         {
             isReturn = true;
-            animator.SetFloat("Speed", -1f);  
-            animator.speed = defaultSpeed;
+            bedAnimator.SetFloat("Speed", -1f);  
+            bedAnimator.speed = defaultSpeed;
         }
     }
 
     public void ResetAnimation()
     {
-        animator.Play(animationName, 0, 0); // Play animation from the beginning
-        animator.speed = defaultSpeed; // Set speed to 0
+        bedAnimator.Play(animationName, 0, 0); // Play animation from the beginning
+        bedAnimator.speed = defaultSpeed; // Set speed to 0
     }
 }
